ecfbaf7e460835db46bb2de6bb65259dd80b8cd7
zoubayerBS <35629400+zoubayerBS@users.noreply.github.com>
2025-11-16 14:51:39 +0100

    Update page header title in page.tsx

--- src/app/(authed)/page.tsx
+++ src/app/(authed)/page.tsx
@@ -150,7 +150,7 @@ export default function Home() {
     <div className="bg-card shadow-sm -mt-4">
       <div className="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
-            <h1 className="text-2xl font-bold tracking-tight text-foreground">Tableau de bord (PostgreSQL)</h1>
+            <h1 className="text-2xl font-bold tracking-tight text-foreground">Tableau de bord </h1>
         </div>
       </div>
     </div>
